e265e32e2a05cb9a298d8cf966c27c1b9d743845
jonasdewinter <jonas.dewinter@student.ucll.be>
2020-12-06 17:26:32 +0100

    schema push

--- coronagevallen/urls.py
+++ coronagevallen/urls.py
@@ -10,7 +10,7 @@ router.register('coronagevallen', views.CoronaView)
 urlpatterns = [
     path('', include(router.urls))
 	path('openapi/', get_schema_view(
-        title="Corona infected persons logger",
-        description="Schema for CRUD operations on Corona infected persons logger API"
+        title="aantal besmette mensen per land",
+        description="schema waarin je kan zien hoeveel mensen er besmet zijn per land"
     ), name='openapi-schema'),
 ]
