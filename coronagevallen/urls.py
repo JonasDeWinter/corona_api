e233ea00889acca18d05f6e2823c9451149f878c
jonasdewinter <jonas.dewinter@student.ucll.be>
2020-12-06 17:30:48 +0100

    schema push

--- coronagevallen/urls.py
+++ coronagevallen/urls.py
@@ -8,7 +8,7 @@ router = routers.DefaultRouter()
 router.register('coronagevallen', views.CoronaView)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
 	path('openapi/', get_schema_view(
         title="aantal besmette mensen per land",
         description="schema waarin je kan zien hoeveel mensen er besmet zijn per land"
